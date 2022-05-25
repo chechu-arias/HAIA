2ff718843dbecd78385ce56fa169869b4f18f45e
chechu-arias <chechuariaswork@gmail.com>
2022-05-25 20:25:36 +0200

    Aplicación

---
+++ app.py
@@ -0,0 +1,87 @@
+
+import os
+
+import json
+import flask
+
+from flask import Flask, render_template, request, redirect, url_for
+
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+
+from transcription import SUBTITLE_LOCAL_DIRECTORY, VIDEO_LOCAL_DIRECTORY, all_work, dummy
+
+os.makedirs(VIDEO_LOCAL_DIRECTORY, exist_ok=True)
+
+
+VIDEO_FILENAME_FORMATS = (
+    '.264', '.3g2', '.3gp', '.3gp2', '.3gpp', '.3gpp2', '.3mm', '.3p2', '.60d', '.787', '.89', '.aaf', '.aec', '.aep', '.aepx',
+    '.aet', '.aetx', '.ajp', '.ale', '.am', '.amc', '.amv', '.amx', '.anim', '.aqt', '.arcut', '.arf', '.asf', '.asx', '.avb',
+    '.avc', '.avd', '.avi', '.avp', '.avs', '.avs', '.avv', '.axm', '.bdm', '.bdmv', '.bdt2', '.bdt3', '.bik', '.bin', '.bix',
+    '.bmk', '.bnp', '.box', '.bs4', '.bsf', '.bvr', '.byu', '.camproj', '.camrec', '.camv', '.ced', '.cel', '.cine', '.cip',
+    '.clpi', '.cmmp', '.cmmtpl', '.cmproj', '.cmrec', '.cpi', '.cst', '.cvc', '.cx3', '.d2v', '.d3v', '.dat', '.dav', '.dce',
+    '.dck', '.dcr', '.dcr', '.ddat', '.dif', '.dir', '.divx', '.dlx', '.dmb', '.dmsd', '.dmsd3d', '.dmsm', '.dmsm3d', '.dmss',
+    '.dmx', '.dnc', '.dpa', '.dpg', '.dream', '.dsy', '.dv', '.dv-avi', '.dv4', '.dvdmedia', '.dvr', '.dvr-ms', '.dvx', '.dxr',
+    '.dzm', '.dzp', '.dzt', '.edl', '.evo', '.eye', '.ezt', '.f4p', '.f4v', '.fbr', '.fbr', '.fbz', '.fcp', '.fcproject',
+    '.ffd', '.flc', '.flh', '.fli', '.flv', '.flx', '.gfp', '.gl', '.gom', '.grasp', '.gts', '.gvi', '.gvp', '.h264', '.hdmov',
+    '.hkm', '.ifo', '.imovieproj', '.imovieproject', '.ircp', '.irf', '.ism', '.ismc', '.ismv', '.iva', '.ivf', '.ivr', '.ivs',
+    '.izz', '.izzy', '.jss', '.jts', '.jtv', '.k3g', '.kmv', '.ktn', '.lrec', '.lsf', '.lsx', '.m15', '.m1pg', '.m1v', '.m21',
+    '.m21', '.m2a', '.m2p', '.m2t', '.m2ts', '.m2v', '.m4e', '.m4u', '.m4v', '.m75', '.mani', '.meta', '.mgv', '.mj2', '.mjp',
+    '.mjpg', '.mk3d', '.mkv', '.mmv', '.mnv', '.mob', '.mod', '.modd', '.moff', '.moi', '.moov', '.mov', '.movie', '.mp21',
+    '.mp21', '.mp2v', '.mp4', '.mp4v', '.mpe', '.mpeg', '.mpeg1', '.mpeg4', '.mpf', '.mpg', '.mpg2', '.mpgindex', '.mpl',
+    '.mpl', '.mpls', '.mpsub', '.mpv', '.mpv2', '.mqv', '.msdvd', '.mse', '.msh', '.mswmm', '.mts', '.mtv', '.mvb', '.mvc',
+    '.mvd', '.mve', '.mvex', '.mvp', '.mvp', '.mvy', '.mxf', '.mxv', '.mys', '.ncor', '.nsv', '.nut', '.nuv', '.nvc', '.ogm',
+    '.ogv', '.ogx', '.osp', '.otrkey', '.pac', '.par', '.pds', '.pgi', '.photoshow', '.piv', '.pjs', '.playlist', '.plproj',
+    '.pmf', '.pmv', '.pns', '.ppj', '.prel', '.pro', '.prproj', '.prtl', '.psb', '.psh', '.pssd', '.pva', '.pvr', '.pxv',
+    '.qt', '.qtch', '.qtindex', '.qtl', '.qtm', '.qtz', '.r3d', '.rcd', '.rcproject', '.rdb', '.rec', '.rm', '.rmd', '.rmd',
+    '.rmp', '.rms', '.rmv', '.rmvb', '.roq', '.rp', '.rsx', '.rts', '.rts', '.rum', '.rv', '.rvid', '.rvl', '.sbk', '.sbt',
+    '.scc', '.scm', '.scm', '.scn', '.screenflow', '.sec', '.sedprj', '.seq', '.sfd', '.sfvidcap', '.siv', '.smi', '.smi',
+    '.smil', '.smk', '.sml', '.smv', '.spl', '.sqz', '.srt', '.ssf', '.ssm', '.stl', '.str', '.stx', '.svi', '.swf', '.swi',
+    '.swt', '.tda3mt', '.tdx', '.thp', '.tivo', '.tix', '.tod', '.tp', '.tp0', '.tpd', '.tpr', '.trp', '.ts', '.tsp', '.ttxt',
+    '.tvs', '.usf', '.usm', '.vc1', '.vcpf', '.vcr', '.vcv', '.vdo', '.vdr', '.vdx', '.veg','.vem', '.vep', '.vf', '.vft',
+    '.vfw', '.vfz', '.vgz', '.vid', '.video', '.viewlet', '.viv', '.vivo', '.vlab', '.vob', '.vp3', '.vp6', '.vp7', '.vpj',
+    '.vro', '.vs4', '.vse', '.vsp', '.w32', '.wcp', '.webm', '.wlmp', '.wm', '.wmd', '.wmmp', '.wmv', '.wmx', '.wot', '.wp3',
+    '.wpl', '.wtv', '.wve', '.wvx', '.xej', '.xel', '.xesc', '.xfl', '.xlmv', '.xmv', '.xvid', '.y4m', '.yog', '.yuv', '.zeg',
+    '.zm1', '.zm2', '.zm3', '.zmv'
+)
+
+@app.route('/', methods=["GET", "POST"])
+def load_index():
+
+    if flask.request.method == 'POST':
+        print(request.form)
+        video_filename = request.files['video']
+        video_extension = "." + video_filename.filename.split(".")[-1]
+        if video_extension not in VIDEO_FILENAME_FORMATS:
+            return render_template('index.jinja2', error="El video no tiene una extensión válida.", show_video=False)
+
+        server_video_filename = secure_filename(video_filename.filename)
+        video_filepath = os.path.join(VIDEO_LOCAL_DIRECTORY, server_video_filename)
+
+        video_filename.save(video_filepath)
+
+        video_lang = request.form.get('video_lang')
+
+        print("VIDEO", video_lang)
+
+        subtitles_lang = request.form.get('subtitles_lang')
+
+        print("SUBS", subtitles_lang)
+
+        #response = all_work(server_video_filename, video_lang, subtitles_lang)
+        response = dummy()
+
+        if response["code"] != 200:
+            return render_template('index.jinja2', error="Se ha producido un error generando los subtítulos.", show_video=False)
+
+        return render_template(
+            'index.jinja2',
+            error="",
+            show_video=True,
+            video_filepath=video_filepath,
+            subtitle_filepath=response["subtitles_file"]
+        )
+
+    return render_template('index.jinja2', error="", show_video=False)
---
+++ transcription.py
@@ -0,0 +1,173 @@
+
+
+from fileinput import filename
+import os
+import time
+import boto3
+import logging
+
+from botocore.exceptions import ClientError
+
+BUCKET_NAME_FOLDER = "proyecto-haia"
+BUCKET_NAME = f"s3://{BUCKET_NAME_FOLDER}/"
+
+FILE_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+
+SUBTITLE_LOCAL_DIRECTORY = os.path.join(FILE_DIRECTORY, "subtitles")
+VIDEO_LOCAL_DIRECTORY = os.path.join(FILE_DIRECTORY, "upload_videos")
+
+def upload_file(local_file_path, file_name):
+    """Upload a file to an S3 bucket
+    :param file_name: File to upload
+    :return: True if file was uploaded, else False
+    """
+
+    local_file = os.path.join(VIDEO_LOCAL_DIRECTORY, file_name)
+
+    s3_client = boto3.client('s3')
+
+    response = s3_client.upload_file(local_file, BUCKET_NAME_FOLDER, file_name)
+
+    return True
+
+def clear_bucket():
+
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(BUCKET_NAME_FOLDER)
+
+    bucket.objects.all().delete()
+
+
+def download_file(path_to_save, file_name):
+
+    local_file = os.path.join(path_to_save, file_name)
+
+    s3 = boto3.client('s3')
+
+    response = s3.download_file(BUCKET_NAME_FOLDER, file_name, local_file)
+
+    return response
+
+
+def transcribe_video(video_filename, video_lang):
+
+    filename_without_extension = video_filename.split(".")[0]
+
+    transcribe = boto3.client('transcribe', 'us-east-1')
+    job_uri = f"{BUCKET_NAME}{video_filename}"
+
+    try:
+        transcribe.delete_transcription_job(TranscriptionJobName=filename_without_extension)
+    except ClientError as e:
+        pass
+
+    transcribe.start_transcription_job(
+        TranscriptionJobName = filename_without_extension,
+        Media = {'MediaFileUri': job_uri
+        },
+        OutputBucketName = BUCKET_NAME_FOLDER,
+        LanguageCode = video_lang,
+        Subtitles = {'Formats': [
+                'srt'
+            ],
+            'OutputStartIndex': 1
+        }
+    )
+
+    while True:
+        status = transcribe.get_transcription_job(TranscriptionJobName=filename_without_extension)
+        if status['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
+            break
+        time.sleep(5)
+
+    print(status)
+
+    return status
+
+def all_work(video_filename, video_lang, subtitle_lang):
+
+    clear_bucket()
+
+    success = upload_file(VIDEO_LOCAL_DIRECTORY, video_filename)
+
+    filename_without_extension = video_filename.split(".")[0]
+
+    json_response = transcribe_video(video_filename, video_lang)
+
+    if json_response == "ERROR" or json_response["TranscriptionJob"]["TranscriptionJobStatus"] != "COMPLETED":
+        return {
+            "code": 404,
+            "message": "ERROR with your petition"
+        }
+
+    subtitle_filename = f"{filename_without_extension}.srt"
+
+    success = download_file(SUBTITLE_LOCAL_DIRECTORY, subtitle_filename)
+
+    lines_to_let_equal = list()
+    lines_to_translate = list()
+
+    f = open(os.path.join(SUBTITLE_LOCAL_DIRECTORY, subtitle_filename), "r")
+    cont = 0
+    for line in f:
+        line = line.replace("\n", "")
+        if cont % 4 == 2:
+            lines_to_translate.append(line)
+        else:
+            lines_to_let_equal.append(line)
+        cont += 1
+    f.close()
+
+    text_to_translate = "\n".join(lines_to_translate)
+
+    translate = boto3.client(service_name='translate', region_name='us-east-1', use_ssl=True)
+
+    result = translate.translate_text(
+        Text=text_to_translate,
+        SourceLanguageCode=video_lang,
+        TargetLanguageCode=subtitle_lang
+    )
+
+    print(result)
+    print(result.get('TranslatedText'))
+
+    text_translated = result.get('TranslatedText').split("\n")
+
+    subtitles_filename = os.path.join(SUBTITLE_LOCAL_DIRECTORY, f"{subtitle_lang}_{subtitle_filename}")
+
+    f = open( subtitles_filename , "w")
+    cont_write = 0
+    index_let_equal = 0
+    index_translated = 0
+    for _ in range(cont):
+        if cont_write % 4 == 2:
+            f.write(text_translated[index_translated] + "\n")
+            index_translated += 1
+        else:
+            f.write(lines_to_let_equal[index_let_equal] + "\n")
+            index_let_equal += 1
+        cont_write += 1
+    f.close()
+
+    return {
+        "code": 200,
+        "message": "All good.",
+        "subtitles_file": subtitles_filename
+    }
+
+def dummy():
+    return {
+        "code": 200,
+        "message": "All good.",
+        "subtitles_file": os.path.join(SUBTITLE_LOCAL_DIRECTORY, f"en-US_es_tortilla_patata.srt")
+    }
+
+
+
+"""
+try:
+        response = s3.download_file(BUCKET_NAME_FOLDER, file_name, file_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+"""
